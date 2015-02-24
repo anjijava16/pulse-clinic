d1b968f4c2815a05d245c55a6112ffd3759ef4ec
Vladimir Shin <vladimir.shin@gmail.com>
2015-02-24 22:55:09 +0900

    Format comments

--- pulse-desktop/src/main/java/com/pulse/desktop/view/panel/ImagePanel.java
+++ pulse-desktop/src/main/java/com/pulse/desktop/view/panel/ImagePanel.java
@@ -5,24 +5,23 @@ import java.awt.Image;
 import javax.swing.ImageIcon;
 import javax.swing.JPanel;
 
+
 /**
- * This is generic panel class, which can hold some 
- * background image. ImagePanel class can be used in 
- * any class, where panel with some background image 
- * is needed.
- * @author befast
+ * This is generic panel class, which can hold some background image. ImagePanel class can be used in
+ * any class, where panel with some background image is needed.
+ * @author Vladimir Shin
  */
-public class ImagePanel 
-    extends JPanel {
+public class ImagePanel
+        extends JPanel {
 
     private Image backgroundImage; // This is the object, which holds some background image.
 
     /**
-     * This is default contructor for the ImagePanel
+     * This is default constructor for the ImagePanel
      * class.
-     * @param String imageFile - This is the filename of the image, which will
-     *                           be used as background image for current ImagePanel
-     *                           instance.
+     * @param imageFile - This is the filename of the image, which will
+     *                    be used as background image for current ImagePanel
+     *                    instance.
      */
     public ImagePanel(String imageFile) {
         this(new ImageIcon(imageFile).getImage());
@@ -30,19 +29,19 @@ public class ImagePanel
 
     /**
      * This is overloaded constructor for the ImagePanel class.
-     * @param Image image - This is the Image instance, which represent the background
-     *                      image for the current ImagePanel instance.
+     * @param image - This is the Image instance, which represent the background
+     *                image for the current ImagePanel instance.
      */
     public ImagePanel(Image image) {
         this.backgroundImage = image;
-        
+
         setSize(this.backgroundImage.getWidth(null), this.backgroundImage.getHeight(null));
     }
 
     /**
      * This function will draw background image in the panel.
-     * @param Graphics g - This class is responsiable for the drawing image in the
-     *                     current ImagePanel instance.
+     * @param g - This class is responsible for the drawing image in the
+     *           current ImagePanel instance.
      */
     @Override
     public void paintComponent(Graphics g) {
