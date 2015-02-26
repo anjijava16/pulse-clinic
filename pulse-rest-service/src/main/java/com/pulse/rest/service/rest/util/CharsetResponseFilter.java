5d0f16b61d83d90ad1d57e1d48a023ee15484b6d
Vladimir Shin <vladimir.shin@gmail.com>
2015-02-26 13:34:07 +0900

    - Update @author label

--- pulse-rest-service/src/main/java/com/pulse/rest/service/rest/util/CharsetResponseFilter.java
+++ pulse-rest-service/src/main/java/com/pulse/rest/service/rest/util/CharsetResponseFilter.java
@@ -1,4 +1,20 @@
+/*
+ * Copyright 2015 Vladimir Shin
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
 package com.pulse.rest.service.rest.util;
+
 
 import com.sun.jersey.spi.container.ContainerRequest;
 import com.sun.jersey.spi.container.ContainerResponse;
@@ -6,6 +22,10 @@ import com.sun.jersey.spi.container.ContainerResponseFilter;
 
 import javax.ws.rs.core.MediaType;
 
+
+/**
+ * @author Vladimir Shin [vladimir.shin@gmail.com]
+ */
 public class CharsetResponseFilter implements ContainerResponseFilter {
 
     public ContainerResponse filter(ContainerRequest request, ContainerResponse response) {
